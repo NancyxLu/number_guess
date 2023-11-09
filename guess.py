ca57296bb77cdd062755a4f10a24bb8cfc70cd96
Nancy Lu <nancy@cloudsoft.com.tw>
2023-11-09 15:54:26 +0800

    game ver1

---
+++ guess.py
@@ -0,0 +1,23 @@
+#產生1~100之間的隨機數
+#讓使用者猜猜看
+#猜對: 恭喜猜對了
+#猜錯: 告訴使用者 要更大或更小 
+
+import random
+r = random.randint(1,100)
+
+print ('猜數字遊戲開始了!')
+number = input('請輸入你猜的數字: ')
+number = int(number)
+while True:
+	if number > r:
+		print ('猜錯嘍，答案比', number, '更小喔')
+		number = input('請重新猜一個數字: ')
+		number = int(number)
+	elif number < r:
+		print ('猜錯嘍，答案比', number, '更大喔')
+		number = input('請重新猜一個數字: ')
+		number = int(number)
+	else :
+		print ('bingo 終於猜對了，恭喜!')
+		break
